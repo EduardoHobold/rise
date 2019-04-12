60964efec2715921b8276ef9f744b301c8c8f9f3
Graaaay <abneralecs@gmail.com>
2019-04-11 21:44:51 -0300

    tetantiva de retornar o bug no pc do cu

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_12_002335) do
+ActiveRecord::Schema.define(version: 2019_04_12_002605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,18 @@ ActiveRecord::Schema.define(version: 2019_04_12_002335) do
     t.string "nome"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "disciplinas", force: :cascade do |t|
+    t.string "nome"
+    t.integer "carga_horaria"
+    t.string "conteudo"
+    t.bigint "semestre_id"
+    t.bigint "curso_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["curso_id"], name: "index_disciplinas_on_curso_id"
+    t.index ["semestre_id"], name: "index_disciplinas_on_semestre_id"
   end
 
   create_table "enderecos", force: :cascade do |t|
@@ -50,5 +62,7 @@ ActiveRecord::Schema.define(version: 2019_04_12_002335) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "disciplinas", "cursos"
+  add_foreign_key "disciplinas", "semestres", column: "semestre_id"
   add_foreign_key "pessoas", "enderecos"
 end

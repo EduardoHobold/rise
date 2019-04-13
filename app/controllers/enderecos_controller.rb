c186517ae752040521069b0f7bd316900e2b329c
Eduardo <hobold.edu@hotmail.com>
2019-04-12 21:39:05 -0300

    Ajustando view enderecos

--- app/controllers/enderecos_controller.rb
+++ app/controllers/enderecos_controller.rb
@@ -69,6 +69,6 @@ class EnderecosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def endereco_params
-      params.require(:endereco).permit(:rua, :bairro, :numero, :cidade, :estado)
+      params.require(:endereco).permit(:rua, :bairro, :numero, :cidade, :estado, :cidade_id)
     end
 end
